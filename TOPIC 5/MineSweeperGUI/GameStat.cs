ba721fdeb051fab23fda180258be9f18e052cdb9
Daniel Louis <DLouis4@my.gcu.edu>
2025-03-30 16:31:54 -0400

    Add files via upload

---
+++ TOPIC 5/MineSweeperGUI/GameStat.cs
@@ -0,0 +1,10 @@
+﻿namespace MineSweeperGUI
+{
+    public class GameStat
+    {
+        public int Id { get; set; }
+        public string PlayerName { get; set; }
+        public int Score { get; set; }
+        public DateTime GameTime { get; set; }
+    }
+}
